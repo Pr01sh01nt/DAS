afb636b0f72cf145aab5c3a7540c18a1d6968a5f
Prashant Kumar <1prashantkumar1@gmail.com>
2024-03-06 08:43:01 +0530

    added auth ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
+
+
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { AuthContextProvider } from "@/context/AuthContext";
+import Navbar from '@/components/shared/Navbar'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -9,9 +12,18 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  console.log("layout renders");
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} border-8 border-cyan-800`}>
+      <AuthContextProvider>
+
+          <Navbar/>
+          {children}
+
+    </AuthContextProvider>
+
+      </body>
     </html>
   );
 }
